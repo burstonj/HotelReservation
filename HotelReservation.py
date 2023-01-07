5bb169f6f444a80ad767a0a60403bb2756bfe623
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-07 10:26:37 -0800

    Revision to code

--- HotelReservation.py
+++ HotelReservation.py
@@ -1,30 +1,32 @@
 from datetime import datetime
 import locale
-
-print("Welcome to the Hotel Reservation program\n")
+#pull and import data
+print("Welcome to the Hotel Reservation program... \n")
 
 again = "y"
 while again.lower() == "y":
-    # get arrival date
+    # arrival date?
     while True:
-        date_str = input("Enter arrival date (YYYY-MM-DD): ")
-        try:
+        date_str = input("Please Enter your arrival date (YYYY-MM-DD): ")
+        try: 
+            # correct format data validation
             arrival_date = datetime.strptime(date_str, "%Y-%m-%d")
         except ValueError:
-            print("Invalid date format. Try again.")
+            print("Invalid date format. Please Try again.")
             print()
-            continue  # skip next if statement and re-start loop
+            continue  # used skip next if statement and re-start loop
 
-        # strip non-zero time values from datetime object
+        # strip non-zero values from datetime object 
         now = datetime.now()
         today = datetime(now.year, now.month, now.day)        
         if arrival_date < today:
-            print("Arrival date must be today or later. Try again.")
+            print("Arrival date must be today or later. Please Try again.")
             print()
         else:
             break
+      # remember to break while 
 
-    # get departure date
+    # ask for their departure date
     while True:
         date_str = input("Please Enter departure date (YYYY-MM-DD): ")
         try:
@@ -35,23 +37,23 @@ while again.lower() == "y":
             continue  # skip next if statement and re-start loop
             
         if departure_date <= arrival_date:
-            print("Departure date must be after arrival date. Try again.")
+            print("Departure date must be after arrival date. Please try again.")
             print()
         else:
             break
-        
+        # remember to break while
     print()
 
-    # calculate nights and cost
-    rate = 85.0
+    # Rate and cost
+    rate = 125.0
     rate_message = ""
-    if arrival_date.month == 8:    # August is high season
-        rate = 105.0
-        rate_message = "(High season)"
+    if arrival_date.month == 8:    # August is peak season
+        rate = 165.0
+        rate_message = "(Peak season rate)"
     total_nights = (departure_date - arrival_date).days
     total_cost = rate * total_nights
 
-    # format results
+    # show formated results
     date_format = "%B %d, %Y"
     locale.setlocale(locale.LC_ALL, "en_US")
     print(f"Arrival Date:    {arrival_date:{date_format}}")
@@ -61,8 +63,7 @@ while again.lower() == "y":
     print(f"Total price:     {locale.currency(total_cost)}")
     print()
 
-    # ask if user wants to continue
-    again = input("Continue? (y/n): ")
+    again = input("Would you like to continue? (y/n): ")
     print()
         
-print("Thank you for your reservation! Bye!")
+print("Thank you for your reservation! Have a great day.")
